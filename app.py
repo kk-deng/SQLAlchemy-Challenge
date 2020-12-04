b58d341c147085ec30a0ec3d7bb325108d0db874
kk-deng <71301799+kk-deng@users.noreply.github.com>
2020-12-03 23:03:03 -0500

    Updated part 2 (app.py)

---
+++ app.py
@@ -0,0 +1,99 @@
+# =========== Import Dependencies ===========
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+from flask import Flask, jsonify
+
+
+#SQLAlchemy 
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Get last date and the most active station
+session = Session(engine)
+record = session.query(Measurement.date, Measurement.prcp).order_by(Measurement.date.desc()).first()
+max_date = dt.date.fromisoformat(record[0])
+one_year = dt.timedelta(days = 365)
+min_date = max_date - one_year
+
+active_station = session.query(Measurement.station, func.count(Measurement.station))\
+    .group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()
+session.close()
+
+#Flask 
+app = Flask(__name__)
+@app.route("/")
+def main():    
+    return 'Welcome to Hawaii API!<br>\
+        <a href="/api/v1.0/precipitation" target="_blank">/api/v1.0/precipitation</a><br>\
+        <a href="/api/v1.0/stations" target="_blank">/api/v1.0/stations</a><br>\
+        <a href="/api/v1.0/tobs" target="_blank">/api/v1.0/tobs</a><br>\
+        <a href="/api/v1.0/20170101" target="_blank">/api/v1.0/20170101</a><br>\
+        <a href="/api/v1.0/20170101/20170801" target="_blank">/api/v1.0/20170101/20170801</a><br>'
+
+@app.route("/api/v1.0/precipitation")
+def prcp_route(): 
+    session = Session(engine)
+    results_1 = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= min_date).all()
+    session.close()
+    return jsonify(dict(results_1))
+
+@app.route("/api/v1.0/stations")
+def station_route(): 
+    session = Session(engine)
+    # results = session.query(Measurement.station).group_by(Measurement.station).all()
+    results_2 = session.query(Station.station, Station.name).all()
+    session.close()
+    return jsonify(dict(results_2))
+
+@app.route("/api/v1.0/tobs")
+def tobs_route(): 
+    session = Session(engine)
+    results_3 = session.query(Measurement.date, Measurement.tobs).filter_by(station = active_station[0]).filter(Measurement.date >= min_date).all()
+    session.close()
+    return jsonify(dict(results_3))
+
+@app.route("/api/v1.0/<start>")
+def start_route(start): 
+    session = Session(engine)
+    start_date = start[:4] + "-" + start[4:6] + "-" + start[-2:]
+    # Filter station?
+    results_4 = session.query(Measurement.tobs).filter(Measurement.date >= start_date).all()
+    dict_results_4 = {
+        "TMin": np.min(results_4),
+        "TAvg": np.mean(results_4),
+        "TMax": np.max(results_4)
+    }
+    session.close()
+    return jsonify(dict_results_4)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_route(start, end): 
+    session = Session(engine)
+    if start >= end:
+        return "Error: Invalid date range!<br>Please make sure the <b>end date</b> is later than <b>start date</b>."
+    else:
+        start_date = start[:4] + "-" + start[4:6] + "-" + start[-2:]
+        end_date = end[:4] + "-" + end[4:6] + "-" + end[-2:]
+
+        results_5 = session.query(Measurement.tobs).filter(Measurement.date >= start_date)\
+            .filter(Measurement.date <= end_date).all()
+        
+        dict_results_5 = {
+            "TMin": np.min(results_5),
+            "TAvg": np.mean(results_5),
+            "TMax": np.max(results_5)
+        }
+        session.close()
+        return jsonify(dict_results_5)
+
+if __name__ == '__main__': 
+    app.run(debug=True)
