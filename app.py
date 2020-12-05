3f591201eb9ad716a50575dec553032a50f2efaa
kk-deng <71301799+kk-deng@users.noreply.github.com>
2020-12-04 20:57:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,22 @@ session.close()
 app = Flask(__name__)
 @app.route("/")
 def main():    
-    return 'Welcome to Hawaii API!<br>\
-        <a href="/api/v1.0/precipitation" target="_blank">/api/v1.0/precipitation</a><br>\
-        <a href="/api/v1.0/stations" target="_blank">/api/v1.0/stations</a><br>\
-        <a href="/api/v1.0/tobs" target="_blank">/api/v1.0/tobs</a><br>\
-        <a href="/api/v1.0/20170101" target="_blank">/api/v1.0/20170101</a><br>\
-        <a href="/api/v1.0/20170101/20170801" target="_blank">/api/v1.0/20170101/20170801</a><br>'
+    return (
+        '<h1>Welcome to Hawaii API!</h1><br>\
+        <h2>This API accepts the following functions:</h2><br>\
+        <ul><li><a href="/api/v1.0/precipitation" target="_blank">/api/v1.0/precipitation</a><br>\
+        Return a JSON list of precipitation data for the <b>last year</b> in the database.</li><br>\
+        <li><a href="/api/v1.0/stations" target="_blank">/api/v1.0/stations</a><br>\
+        Return a JSON list of <b>stations</b> in the database.</li><br>\
+        <li><a href="/api/v1.0/tobs" target="_blank">/api/v1.0/tobs</a><br>\
+        Return a JSON list of temperature observations (TOBS) of the most active station for the last year.</li><br>\
+        <li><a href="/api/v1.0/20170101" target="_blank">/api/v1.0/<b>20170101</b></a><br>\
+        Return a JSON list of the <b>minimum temperature</b>, the <b>average temperature</b>, and the <b>max temperature</b> for a given start date.<br>\
+        Start date must be in <b>YYYYMMDD</b> format.</li><br>\
+        <li><a href="/api/v1.0/20170101/20170801" target="_blank">/api/v1.0/<b>20170101</b>/<b>20170801</b></a><br>\
+        Return a JSON list of the <b>minimum temperature</b>, the <b>average temperature</b>, and the <b>max temperature</b> for a given start-end range.<br>\
+        Start date must be in <b>YYYYMMDD</b> format.</li><br>'
+    )
 
 @app.route("/api/v1.0/precipitation")
 def prcp_route(): 
@@ -65,36 +75,34 @@ def tobs_route():
 def start_route(start): 
     session = Session(engine)
     start_date = start[:4] + "-" + start[4:6] + "-" + start[-2:]
-    # Filter station?
-    results_4 = session.query(Measurement.tobs).filter(Measurement.date >= start_date).all()
-    dict_results_4 = {
-        "TMin": np.min(results_4),
-        "TAvg": np.mean(results_4),
-        "TMax": np.max(results_4)
-    }
+    results_4 = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
     session.close()
-    return jsonify(dict_results_4)
+    return jsonify(
+        TMin=results_4[0][0],
+        TAvg=results_4[0][1],
+        TMax=results_4[0][2]
+    )
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end_route(start, end): 
     session = Session(engine)
     if start >= end:
-        return "<h2>Error: Invalid date range!</h2>\
-            <br>Please make sure the <b>end date</b> is later than <b>start date</b>."
+        return (
+            "<h2>Error: Invalid date range!</h2><br>\
+            Please make sure the <b>end date(" + end + ")</b> is later than <b>start date(" + start +")</b>."
+        )
     else:
         start_date = start[:4] + "-" + start[4:6] + "-" + start[-2:]
         end_date = end[:4] + "-" + end[4:6] + "-" + end[-2:]
-
-        results_5 = session.query(Measurement.tobs).filter(Measurement.date >= start_date)\
-            .filter(Measurement.date <= end_date).all()
-        
-        dict_results_5 = {
-            "TMin": np.min(results_5),
-            "TAvg": np.mean(results_5),
-            "TMax": np.max(results_5)
-        }
+        results_5 = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
         session.close()
-        return jsonify(dict_results_5)
+        return jsonify(
+            TMin=results_5[0][0],
+            TAvg=results_5[0][1],
+            TMax=results_5[0][2]
+        )
 
 if __name__ == '__main__': 
     app.run(debug=True)
