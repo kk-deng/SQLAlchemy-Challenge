e82626e82fb05840ff1753840452547b7ba6c009
kk-deng <71301799+kk-deng@users.noreply.github.com>
2020-12-05 23:09:32 -0500

    Updated with final files

--- app.py
+++ app.py
@@ -9,26 +9,28 @@ from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
 
 
-#SQLAlchemy 
+# SQLAlchemy 
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
+# Map tables
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Get last date and the most active station
+# Get last date 
 session = Session(engine)
 record = session.query(Measurement.date, Measurement.prcp).order_by(Measurement.date.desc()).first()
 max_date = dt.date.fromisoformat(record[0])
 one_year = dt.timedelta(days = 365)
 min_date = max_date - one_year
 
+# Get the most active station
 active_station = session.query(Measurement.station, func.count(Measurement.station))\
     .group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()
 session.close()
 
-#Flask 
+# Flask 
 app = Flask(__name__)
 @app.route("/")
 def main():    
@@ -46,45 +48,56 @@ def main():
         Start date must be in <b>YYYYMMDD</b> format.</li><br>\
         <li><a href="/api/v1.0/20170101/20170801" target="_blank">/api/v1.0/<b>20170101</b>/<b>20170801</b></a><br>\
         Return a JSON list of the <b>minimum temperature</b>, the <b>average temperature</b>, and the <b>max temperature</b> for a given start-end range.<br>\
-        Start/end dates must be in <b>YYYYMMDD</b> format.</li><br>'
+        Start/end dates must be in <b>YYYYMMDD</b> format. And end date is <b>later</b> than start date.</li>'
     )
 
+# Precipitation route
 @app.route("/api/v1.0/precipitation")
 def prcp_route(): 
     session = Session(engine)
-    results_1 = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= min_date).all()
+    results_1 = session.query(Measurement.date, Measurement.prcp)\
+                .filter(Measurement.date >= min_date).all()
     session.close()
+
     return jsonify(dict(results_1))
 
+# Stations route
 @app.route("/api/v1.0/stations")
 def station_route(): 
     session = Session(engine)
-    # results = session.query(Measurement.station).group_by(Measurement.station).all()
     results_2 = session.query(Station.station, Station.name).all()
     session.close()
+
     return jsonify(dict(results_2))
 
+# Tobs route
 @app.route("/api/v1.0/tobs")
 def tobs_route(): 
     session = Session(engine)
     results_3 = session.query(Measurement.date, Measurement.tobs)\
                 .filter_by(station = active_station[0]).filter(Measurement.date >= min_date).all()
     session.close()
+
     return jsonify(dict(results_3))
 
+# Tobs with start date route
 @app.route("/api/v1.0/<start>")
 def start_route(start): 
     session = Session(engine)
+    
+    # Add the '-' to entered dates for comparison of YYYY-MM-DD
     start_date = start[:4] + "-" + start[4:6] + "-" + start[-2:]
-    results_4 = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-                filter(Measurement.date >= start_date).all()
+    results_4 = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+                .filter(Measurement.date >= start_date).all()
     session.close()
+
     return jsonify(
         TMin=results_4[0][0],
         TAvg=results_4[0][1],
         TMax=results_4[0][2]
     )
 
+# Tobs with start/end dates route
 @app.route("/api/v1.0/<start>/<end>")
 def start_end_route(start, end): 
     session = Session(engine)
@@ -94,16 +107,20 @@ def start_end_route(start, end):
             Please make sure the <b>end date(" + end + ")</b> is later than <b>start date(" + start +")</b>."
         )
     else:
+        # Add the '-' to entered dates for comparison of YYYY-MM-DD
         start_date = start[:4] + "-" + start[4:6] + "-" + start[-2:]
         end_date = end[:4] + "-" + end[4:6] + "-" + end[-2:]
-        results_5 = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-                    filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+        results_5 = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+                    .filter(Measurement.date >= start_date)\
+                    .filter(Measurement.date <= end_date).all()
         session.close()
+
         return jsonify(
             TMin=results_5[0][0],
             TAvg=results_5[0][1],
             TMax=results_5[0][2]
         )
 
+# Main run
 if __name__ == '__main__': 
     app.run(debug=True)
